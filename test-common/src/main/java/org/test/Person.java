517b3269f56da382878316617e1057d070628c3d
unknown <yake2002@sina.com>
2018-02-03 14:23:20 +0800

    first commit

---
+++ test-common/src/main/java/org/test/Person.java
@@ -0,0 +1,27 @@
+package org.test;
+
+public class Person {
+	private String name = "小王";
+	private String sex = "男";
+	private String weight = "150斤";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	
+	
+}
---
+++ test-service/src/main/java/org/test/TestPerson.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p = new Person();
+		
+		System.out.println("姓名:"+p.getName());
+		System.out.println("性别:"+p.getSex());
+		System.out.println("年龄:"+p.getWeight());
+	}
+
+}
